4e50e8a3ce088b264a5d571c632b6eb8d352b954
Rishith Reddy <babarishith@gmail.com>
2016-11-26 00:51:09 +0530

    basic overview

---
+++ main.m
@@ -0,0 +1,13 @@
+%Input video and initial processing
+
+%Detect Object and get mask
+
+%seperate object and background
+
+%detect pivot
+
+%get mouse input and angle
+
+%transform object 
+
+%combine object and background in video
